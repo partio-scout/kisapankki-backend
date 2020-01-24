97867629c23a14e9c466b98024c628798fddbf52
Arttu Janhunen <arttu.janhunen@gmail.com>
2020-01-24 02:20:21 +0200

    Started working on user model and user router

---
+++ controllers/userRouter.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcrypt')
+const userRouter = require('express').Router()
+const User = require('../models/user')
+
+userRouter.post('/', async (req, res, next) => {
+  try {
+
+
+    const body = req.body
+
+    const saltRounds = 10
+    const password = await bcrypt.hash(body.password, saltRounds)
+
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      password
+    })
+
+    const savedUser = await user.save()
+
+    res.json(savedUser.toJSON())
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+module.exports = userRouter
---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  username: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    delete returnedObject._id
+    delete returnedObject._v
+    delete returnedObject.password
+  }
+})
+
+userSchema.plugin(uniqueValidator)
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
