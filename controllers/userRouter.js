dfdbd6a18b741313f055686fbc85e936697c53c3
Arttu Janhunen <arttu.janhunen@gmail.com>
2020-01-25 15:03:12 +0200

    Created test user creating a user

--- controllers/userRouter.js
+++ controllers/userRouter.js
@@ -2,6 +2,16 @@ const bcrypt = require('bcrypt')
 const userRouter = require('express').Router()
 const User = require('../models/user')
 
+
+userRouter.get('/', async (req,res,next)=>{
+  try{
+    const users = await User.find({})
+    res.json(users.map((user)=> user.toJSON()))
+  }catch(exception){
+    next(exception)
+  }
+})
+
 userRouter.post('/', async (req, res, next) => {
 
   const body = req.body
