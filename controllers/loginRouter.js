ef52beecc806efc01f4e40830923bd80f4730451
Arttu Janhunen <arttu.janhunen@gmail.com>
2020-01-28 10:28:44 +0200

    clean loginrouter

--- controllers/loginRouter.js
+++ controllers/loginRouter.js
@@ -7,28 +7,26 @@ loginRouter.post('/', async (req, res, next) => {
   const body = req.body
 
   const user = await User.findOne({ username: body.username })
-  console.log(body.password)
-  console.log(user)
   const passwordCorrect = user === null
     ? false
     : await bcrypt.compare(body.password, user.password)
 
-    if (!(user && passwordCorrect)){
-      return res.status(401).json({
-        error: 'invalid username or password'
-      })
-    }
+  if (!(user && passwordCorrect)) {
+    return res.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
 
-    const userForToken = {
-      username: user.username,
-      id: user._id
-    }
+  const userForToken = {
+    username: user.username,
+    id: user._id
+  }
 
-    const token = jwt.sign(userForToken, process.env.SECRET)
+  const token = jwt.sign(userForToken, process.env.SECRET)
 
-    res
+  res
     .status(200)
-    .send({token, username: user.username, name: user.name})
+    .send({ token, username: user.username, name: user.name })
 })
 
 module.exports = loginRouter
