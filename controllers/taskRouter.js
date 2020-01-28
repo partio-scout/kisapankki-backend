df807f3eb29418974f0d828ca205dd8d165942ca
Tattimus <samu.tatti@luukku.com>
2020-01-28 14:20:50 +0200

    populate pointers, pending set with jwt

--- controllers/taskRouter.js
+++ controllers/taskRouter.js
@@ -1,9 +1,23 @@
 const taskRouter = require('express').Router()
 const Task = require('../models/task')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = (req) => {
+    const auth = req.get('authorization')
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+        return auth.substring(7)
+    }
+    return null
+}
 
 taskRouter.get('/', async (req, res, next) => {
     try {
         const tasks = await Task.find({})
+        .populate('ageGroup')
+        .populate('category')
+        .populate('language')
+        .populate('rules')
+        .exec()
         res.json(tasks.map((task) => task.toJSON()))
     } catch (exception) {
         next(exception)
@@ -12,6 +26,12 @@ taskRouter.get('/', async (req, res, next) => {
 
 taskRouter.post('/', async (req, res, next) => {
     const body = req.body
+    const pen = false
+    const token = getTokenFrom(req)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (token && decodedToken.id) {
+        pen = true
+    }
     const task = new Task({
         name: body.name,
         assignmentText: body.assignmentText,
@@ -19,7 +39,7 @@ taskRouter.post('/', async (req, res, next) => {
         gradingScale: body.gradingScale,
         creatorName: body.creatorName,
         creatorEmail: body.creatorEmail,
-        pending: true,
+        pending: pen,
         ageGroup: body.ageGroup,
         category: body.category,
         language: body.language,
