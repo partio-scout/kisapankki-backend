a62c2415d78cc4e2f1c1ae260087a98ac7b3346f
Tattimus <samu.tatti@luukku.com>
2020-03-09 14:05:48 +0200

    delete and modify for rules

--- controllers/ruleRouter.js
+++ controllers/ruleRouter.js
@@ -1,5 +1,15 @@
 const ruleRouter = require('express').Router()
 const Rule = require('../models/rule')
+const jwt = require('jsonwebtoken')
+const Task = require('../models/task')
+
+const getTokenFrom = (req) => {
+  const auth = req.get('authorization')
+  if (auth && auth.toLowerCase().startsWith('bearer ')) {
+    return auth.substring(7)
+  }
+  return null
+}
 
 ruleRouter.get('/', async (req, res, next) => {
   try {
@@ -13,18 +23,80 @@ ruleRouter.get('/', async (req, res, next) => {
 })
 
 ruleRouter.post('/', async (req, res, next) => {
-  const { body } = req
+  if (req.get('authorization')) {
+    const token = getTokenFrom(req)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (token && decodedToken.id) {
+      const { body } = req
 
-  const rule = new Rule({
-    name: body.rules,
-    task: [],
-  })
+      const rule = new Rule({
+        name: body.rules,
+        task: [],
+      })
 
-  try {
-    const savedRule = await rule.save()
-    res.json(savedRule.toJSON())
-  } catch (exception) {
-    next(exception)
+      try {
+        const savedRule = await rule.save()
+        res.json(savedRule.toJSON())
+      } catch (exception) {
+        next(exception)
+      }
+    } else {
+      res.status(401).end()
+    }
+  } else {
+    res.status(401).end()
+  }
+})
+
+ruleRouter.delete('/:id', async (req, res, next) => {
+  if (req.get('authorization')) {
+    const token = getTokenFrom(req)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (token && decodedToken.id) {
+      try {
+        const id = req.params.id
+        const delRules = await Rule.findById(id)
+        if (delRules) {
+          const tasksWithPointer = await Task.find({ _id: { $in: delRules.task } })
+          tasksWithPointer.forEach(async (t) => {
+            t.rules = null
+            t.update({ new: true })
+          })
+          await delRules.remove()
+          res.status(204).end()
+        }
+      } catch (exception) {
+        next(exception)
+      }
+    } else {
+      res.status(401).end()
+    }
+  } else {
+    res.status(401).end()
+  }
+})
+
+ruleRouter.put('/:id', async (req, res, next) => {
+  if (req.get('authorization')) {
+    const token = getTokenFrom(req)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (token && decodedToken.id) {
+      try {
+        const { body } = req
+        const updRules = await Rule.findById(req.params.id)
+        if (updRules) {
+          updRules.name = body.name
+          const updatedRules = await updRules.save()
+          res.json(updatedRules.toJSON())
+        }
+      } catch (exception) {
+        next(exception)
+      }
+    } else {
+      res.status(401).end()
+    }
+  } else {
+    res.status(401).end()
   }
 })
 
--- tests/rule_api.test.js
+++ tests/rule_api.test.js
@@ -2,14 +2,38 @@ const supertest = require('supertest')
 const mongoose = require('mongoose')
 const app = require('../app')
 const Rule = require('../models/rule')
+const User = require('../models/user')
 
 const api = supertest(app)
 
-beforeEach(async () => {
-  await Rule.deleteMany({})
-})
-
 describe('Rules', () => {
+
+  let token = null
+
+  beforeAll(async () => {
+    const newAdmin = {
+      name: 'testAdminN',
+      username: 'testAdminUN',
+      password: 'testAdminPW',
+      adminKey: process.env.ADMIN_KEY,
+    }
+    await api
+      .post('/api/user/adminkey')
+      .send(newAdmin)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const loggedInAdmin = await api
+      .post('/api/login')
+      .send(newAdmin)
+      .expect(200)
+    token = loggedInAdmin.body.token
+  })
+
+  beforeEach(async () => {
+    await Rule.deleteMany({})
+  })
+
   test('can be fetched', async () => {
     const ruleOne = new Rule({
       name: 'jokamies',
@@ -34,11 +58,11 @@ describe('Rules', () => {
   })
 
   test('can be added', async () => {
-    await Rule.deleteMany({})
 
     await api
       .post('/api/rule')
       .send({ rules: 'Viralliset' })
+      .set('authorization', `bearer ${token}`)
       .expect(200)
       .expect('Content-type', /application\/json/)
 
@@ -46,8 +70,52 @@ describe('Rules', () => {
 
     expect(rules[0].name).toBe('Viralliset')
   })
+
+  test('can be deleted', async () => {
+    await api
+      .post('/api/rule')
+      .send({ rules: 'Modernit' })
+      .set('authorization', `bearer ${token}`)
+      .expect(200)
+      .expect('Content-type', /application\/json/)
+
+    const rules = await Rule.find({})
+    expect(rules[0].name).toBe('Modernit')
+    expect(rules.length).toBe(1)
+
+    await api
+      .delete(`/api/rule/${rules[0].id}`)
+      .set('authorization', `bearer ${token}`)
+      .expect(204)
+
+    const rulesAD = await Rule.find({})
+    expect(rulesAD.length).toBe(0)
+  })
+
+  test('can be modified', async () => {
+    await api
+      .post('/api/rule')
+      .send({ rules: 'Paikalliset' })
+      .set('authorization', `bearer ${token}`)
+      .expect(200)
+      .expect('Content-type', /application\/json/)
+    const rules = await Rule.find({})
+    expect(rules[0].name).toBe('Paikalliset')
+    expect(rules.length).toBe(1)
+
+    await api
+      .put(`/api/rule/${rules[0].id}`)
+      .send({ name: 'Kansainväliset' })
+      .set('authorization', `bearer ${token}`)
+      .expect(200)
+      .expect('Content-type', /application\/json/)
+    const rulesAM = await Rule.find({})
+    expect(rulesAM[0].name).toBe('Kansainväliset')
+    expect(rules.length).toBe(1)
+  })
 })
 
-afterAll(() => {
+afterAll(async () => {
+  await User.deleteMany({})
   mongoose.connection.close()
 })
