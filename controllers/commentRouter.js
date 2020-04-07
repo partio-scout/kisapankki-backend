759b47ee0fa08ac2dad5bc513be39a0b32a6f4eb
NooraVino <noora.virolainen@yahoo.com>
2020-04-07 17:21:04 +0300

    router for Comments

---
+++ controllers/commentRouter.js
@@ -0,0 +1,32 @@
+const commentRouter = require('express').Router()
+const Comment = require('../models/comment')
+const jwt = require('jsonwebtoken')
+
+
+commentRouter.get('/', async (req, res, next) => {
+  try {
+    const comments = await Comment.find({})
+    res.json(comments.map((comment) => comment.toJSON()))
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+commentRouter.post('/', async (req, res, next) => {
+  const { body } = req
+  const comment = new Comment({
+    content: body.content,
+    nickName: body.nickName,
+    created: body.created,
+    pending: true,
+    task: [],
+  })
+  try {
+    const savedComment = await comment.save()
+    res.json(savedComment.toJSON())
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+module.exports = commentRouter
